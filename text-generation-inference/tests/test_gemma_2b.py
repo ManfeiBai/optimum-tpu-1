4fcbf60f31c503bb8f88f3df2f70864c93853f05
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2024-06-04 00:40:21 -0700

    Update test_gemma_2b.py

--- text-generation-inference/tests/test_gemma_2b.py
+++ text-generation-inference/tests/test_gemma_2b.py
@@ -55,10 +55,12 @@ def create_request(
 
 
 def iter_single_text(input_text_i):
+    print("enter iter_single_text")
     input_text = input_text_i# "It was a bright cold day in April, and the clocks were striking thirteen."
     max_new_tokens = 20
     # generated_text = "\n\nThe first thing I noticed was the smell of the rain. It was a smell I had never"
 
+    print("before generator")
     generator = TpuGenerator.from_pretrained(
         model_path, revision="", max_batch_size=1, max_sequence_length=SEQUENCE_LENGTH
     )
@@ -66,47 +68,53 @@ def iter_single_text(input_text_i):
     batch = Batch(id=0, requests=[request], size=1, max_tokens=SEQUENCE_LENGTH)
     generations, next_batch = generator.prefill(batch)
     # We already generated one token: call decode max_new_tokens - 1 times
+    print("before for _ in tqdm")
     for _ in tqdm(range(max_new_tokens - 1)):
+        print("enter for _ in tqdm(")
         assert next_batch.size == 1
         assert next_batch.max_tokens == 1024
         assert len(generations) == 1
         assert len(generations[0].tokens.ids) == 1
         generations, next_batch = generator.decode([next_batch])
+        print("finish one for _ in tqdm iter")
+    print("finish all for _ in tqdm")
     assert next_batch is None
     assert len(generations) == 1
     output = generations[0].generated_text
+    print("get output")
     # assert output.generated_tokens == max_new_tokens
     # assert output.finish_reason == 0
     # assert output.text == generated_text
 
 
-def test_decode_single(model_path):
-    input_text = "It was a bright cold day in April, and the clocks were striking thirteen."
-    max_new_tokens = 20
-    generated_text = "\n\nThe first thing I noticed was the smell of the rain. It was a smell I had never"
+# def test_decode_single(model_path):
+#     input_text = "It was a bright cold day in April, and the clocks were striking thirteen."
+#     max_new_tokens = 20
+#     generated_text = "\n\nThe first thing I noticed was the smell of the rain. It was a smell I had never"
 
-    generator = TpuGenerator.from_pretrained(
-        model_path, revision="", max_batch_size=1, max_sequence_length=SEQUENCE_LENGTH
-    )
-    request = create_request(id=0, inputs=input_text, max_new_tokens=max_new_tokens, do_sample=False)
-    batch = Batch(id=0, requests=[request], size=1, max_tokens=SEQUENCE_LENGTH)
-    generations, next_batch = generator.prefill(batch)
-    # We already generated one token: call decode max_new_tokens - 1 times
-    for _ in tqdm(range(max_new_tokens - 1)):
-        assert next_batch.size == 1
-        assert next_batch.max_tokens == 1024
-        assert len(generations) == 1
-        assert len(generations[0].tokens.ids) == 1
-        generations, next_batch = generator.decode([next_batch])
-    assert next_batch is None
-    assert len(generations) == 1
-    output = generations[0].generated_text
-    assert output.generated_tokens == max_new_tokens
-    assert output.finish_reason == 0
-    assert output.text == generated_text
+#     generator = TpuGenerator.from_pretrained(
+#         model_path, revision="", max_batch_size=1, max_sequence_length=SEQUENCE_LENGTH
+#     )
+#     request = create_request(id=0, inputs=input_text, max_new_tokens=max_new_tokens, do_sample=False)
+#     batch = Batch(id=0, requests=[request], size=1, max_tokens=SEQUENCE_LENGTH)
+#     generations, next_batch = generator.prefill(batch)
+#     # We already generated one token: call decode max_new_tokens - 1 times
+#     for _ in tqdm(range(max_new_tokens - 1)):
+#         assert next_batch.size == 1
+#         assert next_batch.max_tokens == 1024
+#         assert len(generations) == 1
+#         assert len(generations[0].tokens.ids) == 1
+#         generations, next_batch = generator.decode([next_batch])
+#     assert next_batch is None
+#     assert len(generations) == 1
+#     output = generations[0].generated_text
+#     assert output.generated_tokens == max_new_tokens
+#     assert output.finish_reason == 0
+#     assert output.text == generated_text
 
 
 def test_decode_multi(model_path):
+    print("before all tests for gemma-2b")
     prompts: List[str] = [
       "I believe the meaning of life is",
       "To add an element to an ArrayList of a specific class type in Java, you can follow the following steps:\n\n1. Create an instance of the class to be added.\n2. Get a reference to the ArrayList.\n3. Call the `add()` method on the ArrayList, passing the instance of the class as the argument.\n\nHere's an example of how to add an object of type `Person` to an ArrayList of type `ArrayList<Person>`:\n```csharp\n// Create a new instance of the Person class\nPerson person = new Person(\"John\", 25);\n\n// Get a reference to the ArrayList\nArrayList<Person> peopleList = new ArrayList<>();\n\n// Add the person object to the ArrayList\npeopleList.add(person);\n```\nIn this example, the `Person` class is assumed to have a constructor that takes two arguments: a String for the person's name, and an int for their age. You can substitute your own class and constructor as necessary.",
@@ -114,7 +122,9 @@ def test_decode_multi(model_path):
       "<s>[INST] <<SYS>>\nYou are an AI assistant that helps people find information. Provide a detailed answer so user don\u2019t need to search outside to understand the answer.\n<</SYS>>\n\nUse reasoning to lead to the answer of the following question:\nWhere are you likely to find water underneath?\nOptions:\n- toilet\n- sink\n- jar\n- bridge\n- house\n Reasoning process: [/INST",
       "<s>[INST] <<SYS>>\nYou are an AI assistant. You will be given a task. You must generate a detailed and long answer.\n<</SYS>>\n\nContinue the following story.\n\nKay didn't have shoes that fit her feet properly. She only wore sneakers, because the \nChoose from: [I] shoes  fitted badly. [II] sneakers  fitted badly. [/INST]",
     ]
+    print("has prompts")
     for prompt in prompts:
+        print("enter for loop")
         iter_single_text(prompt)
     print("finish all tests for gemma-2b")
     
