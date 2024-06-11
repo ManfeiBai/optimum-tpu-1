526233d8747f5882eb9b407f8c5ab6f31671f659
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2024-06-10 23:19:30 -0700

    Update test_gemma_7b.py

--- text-generation-inference/tests/test_gemma_7b.py
+++ text-generation-inference/tests/test_gemma_7b.py
@@ -53,29 +53,73 @@ def create_request(
     stopping_parameters = StoppingCriteriaParameters(max_new_tokens=max_new_tokens)
     return Request(id=id, inputs=inputs, parameters=parameters, stopping_parameters=stopping_parameters)
 
+def test_run_decode_multi_all():
+  os.environ["HF_SEQUENCE_LENGTH"] = str(SEQUENCE_LENGTH)
+  model_path = fetch_model(MODEL_ID)
+  # print("in test_decode_multi, model_path is: ", model_path)
+  run_decode_multi(model_path)
 
-@pytest.mark.slow
-def test_decode_single(model_path):
-    input_text = "It was a bright cold day in April, and the clocks were striking thirteen."
-    max_new_tokens = 20
-    generated_text = "\n\nThe time is 1984. The place is Airstrip One, the British"
-
+# @pytest.mark.slow
+def run_decode_multi(model_path):
     generator = TpuGenerator.from_pretrained(
-        model_path, revision="", max_batch_size=1, max_sequence_length=SEQUENCE_LENGTH
+        model_path, revision="", max_batch_size=1,
+        max_sequence_length=SEQUENCE_LENGTH
     )
-    request = create_request(id=0, inputs=input_text, max_new_tokens=max_new_tokens, do_sample=False)
-    batch = Batch(id=0, requests=[request], size=1, max_tokens=SEQUENCE_LENGTH)
-    generations, next_batch = generator.prefill(batch)
-    # We already generated one token: call decode max_new_tokens - 1 times
-    for _ in tqdm(range(max_new_tokens - 1)):
-        assert next_batch.size == 1
-        assert next_batch.max_tokens == 128
+
+    start_time = time.time()
+    prompts: List[str] = [
+      "I believe the meaning of life is",
+      "To add an element to an ArrayList of a specific class type in Java",
+      "you can follow the following steps:.",
+      "Create an instance of the class to be added.\n2.",
+      "Get a reference to the ArrayList.\n3.", 
+      "Call the `add()` method on the ArrayList,",
+      "passing the instance of the class as the argument.",
+      "Here's an example of how to add an object of type `Person` to an ArrayList of type `ArrayList<Person>`:",
+      "```csharp\n// Create a new instance of the Person class\nPerson person = new Person(\"John\", 25);",
+      "// Get a reference to the ArrayList\nArrayList<Person> peopleList = new ArrayList<>();",
+      "Add the person object to the ArrayList peopleList",
+      "In this example, the `Person` class is assumed to have a constructor that takes two arguments:",
+      "a String for the person's name, and an int for their age. You can substitute your own class and constructor as necessary.",
+      "You are an AI assistant. User will you give you a task.",
+      "Your goal is to complete the task as faithfully as you can.",
+      "While performing the task think step-by-step and justify your steps.\n<</SYS>>",
+      "Question 1: What is commercial real estate finance?",
+      "Question 2: What are Commercial Real Estate services?",
+      "Options are:\n[a]. no.\n[b]. yes.\nWould the answer to these two questions be the same?",
+      "You are an AI assistant that helps people find information.",
+      "Provide a detailed answer so user don\u2019t need to search outside to understand the answer.",
+      "Use reasoning to lead to the answer of the following question:\nWhere are you likely to find water underneath?",
+      "Options:\n- toilet\n- sink\n- jar\n- bridge\n- house\n Reasoning process:",
+      "You are an AI assistant. You will be given a task. You must generate a detailed and long answer.",
+      "Continue the following story.\n\nKay didn't have shoes that fit her feet properly.",
+      "She only wore sneakers, because the \nChoose from: [I] shoes  fitted badly. [II] sneakers  fitted badly",
+    ]
+    # input_text = "It was a bright cold day in April, and the clocks were striking thirteen."
+    max_new_tokens = 20
+    # generated_text = "\n\nThe time is 1984. The place is Airstrip One, the British"
+
+    # generator = TpuGenerator.from_pretrained(
+    #     model_path, revision="", max_batch_size=1,
+    #     max_sequence_length=SEQUENCE_LENGTH
+    # )
+    for prompt in prompts:
+        input_text = prompt
+        request = create_request(id=0, inputs=input_text, max_new_tokens=max_new_tokens, do_sample=False)
+        batch = Batch(id=0, requests=[request], size=1, max_tokens=SEQUENCE_LENGTH)
+        generations, next_batch = generator.prefill(batch)
+        # We already generated one token: call decode max_new_tokens - 1 times
+        for _ in tqdm(range(max_new_tokens - 1)):
+            assert next_batch.size == 1
+            assert next_batch.max_tokens == 128
+            assert len(generations) == 1
+            assert len(generations[0].tokens.ids) == 1
+            generations, next_batch = generator.decode([next_batch])
+        assert next_batch is None
         assert len(generations) == 1
-        assert len(generations[0].tokens.ids) == 1
-        generations, next_batch = generator.decode([next_batch])
-    assert next_batch is None
-    assert len(generations) == 1
-    output = generations[0].generated_text
-    assert output.generated_tokens == max_new_tokens
-    assert output.finish_reason == 0
-    assert output.text == generated_text
+        output = generations[0].generated_text
+        generator.clear()
+    print("--- finish all requests used : %s seconds ---" % (time.time() - start_time))
+        # assert output.generated_tokens == max_new_tokens
+        # assert output.finish_reason == 0
+        # assert output.text == generated_text
